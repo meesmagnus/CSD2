452b9f14e14e5467f1f4b5a754e8c7d6b8f41fca
meesmagnus <meesmagnus@gmail.com>
2025-10-20 09:50:35 +0200

    deleted duplicates

--- CSD2a/eindopdracht/random.choicesTest.py
+++
@@ -1,8 +0,0 @@
-import random
-
-weights = [0.1, 0.1, 0.8]
-# list = [appel, janneke, toon]
-
-randTest = random.choices(population=range(3), weights=weights, k=10)
-# print(list(range(3)))
-print(randTest)
